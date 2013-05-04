767bff595f1d3631566fa1b63dde488aa86d72b2
Ivan Aleksanov <zloi.gremlin@gmail.com>
2013-05-04 14:18:16 +0400

    tort sslider

--- agentstvo/static/js/main.js
+++ agentstvo/static/js/main.js
@@ -6,8 +6,49 @@ $(function () {
 
     $('.lbox').iLightBox({
         skin:'dark',
-        path: 'horizontal',
+        path: 'vertical',
         infinite: true
+    });
+
+    $comments = $('<div class="comments"></div>');
+
+    $('.lbox-comment').iLightBox({
+        skin:'dark rightcom',
+        path: 'vertical',
+        infinite: true,
+        styles: {
+            pageOffsetX: 300,
+            nextOffsetX: 15,
+            nextOpacity: 0,
+            prevOffsetX: 15,
+            prevOpacity: 0
+        },
+        callback: {
+            onOpen: function(){
+                $('body').append($comments);
+
+                var t = this,
+                    id = t.itemsObject[t.vars.current].data('cake');
+                $comments.html($('#cake-'+id).find('.info').html());
+            },
+            onHide: function(){
+                $comments.hide().remove();
+            },
+            onAfterChange: function(api){
+                var t = this,
+                    id = t.itemsObject[api.currentItem].data('cake');
+                $comments.html($('#cake-'+id).find('.info').html());
+            },
+            onAfterLoad: function(api){
+                $comments.fadeIn(180);
+            },
+            onEnterFullScreen: function(){
+                $comments.hide();
+            },
+            onExitFullScreen: function(){
+                $comments.show();
+            }
+        }
     });
 
 });
